151a9d99219eee244f5a70420461839e469c5ec7
kamijin_fanta <kamijin@live.jp>
2018-02-14 00:55:56 +0900

    add types

--- packages/vx__mock-data/index.d.ts
+++ packages/vx__mock-data/index.d.ts
@@ -3,3 +3,23 @@ export interface DateValue {
   value: number;
 }
 export function genDateValue(n: number): DateValue[];
+
+export function genRandomNormalPoints(count: number): [number, number, number][];
+
+export interface Bin {
+  bin: number;
+  count: number;
+}
+export interface BinFunc {
+  (index: number, count: number): number;
+}
+export interface CountFunc {
+  (index: number, count: number): number;
+}
+export function genBin(n: number, bin?: BinFunc, count?: CountFunc): Bin[];
+
+export interface Bins {
+  bin: number;
+  bins: Bin[];
+}
+export function genBins(x: number, y: number, bin?: BinFunc, count?: CountFunc): Bins[];
--- packages/vx__mock-data/index.test.ts
+++ packages/vx__mock-data/index.test.ts
@@ -7,3 +7,40 @@ it('genDateValue', () => {
   expect(mock[0]).toHaveProperty('date');
   expect(mock[0]).toHaveProperty('value');
 });
+
+it('genRandomNormalPoints', () => {
+  let res = mockData.genRandomNormalPoints(10);
+  expect(res).toHaveLength(30);
+  expect(typeof res[0][0]).toEqual('number');
+  expect(typeof res[0][1]).toEqual('number');
+  expect(typeof res[0][2]).toEqual('number');
+});
+
+it('genBin', () => {
+  let res = mockData.genBin(10);
+  expect(typeof res[0].bin).toEqual('number');
+  expect(typeof res[0].count).toEqual('number');
+});
+
+it('genBins', () => {
+  let res = mockData.genBins(10, 10);
+  expect(res).toHaveLength(10);
+  expect(typeof res[0].bin).toEqual('number');
+  expect(res[0].bins).toHaveLength(10);
+  expect(typeof res[0].bins[0].bin).toEqual('number');
+  expect(typeof res[0].bins[0].count).toEqual('number');
+});
+
+it('genStats', () => {
+  let res = mockData.genBins(10, 10);
+  expect(res).toHaveLength(10);
+  expect(typeof res[0].bin).toEqual('number');
+  expect(res[0].bins).toHaveLength(10);
+  expect(typeof res[0].bins[0].bin).toEqual('number');
+  expect(typeof res[0].bins[0].count).toEqual('number');
+});
+
+it('hoge', () => {
+  let res = (mockData as any).genStats(10);
+  console.log(res);
+});
